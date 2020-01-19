ba62ed207f244d9f2295aa80f3711d68396dfd92
KETAN JAIN <ketan1804@rediffmail.com>
2020-01-20 00:12:20 +0530

    adding more files

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -5,6 +5,7 @@ public class LoginPage {
 	public void LoginPage1()
 	{
 		System.out.println("Login_Page1");
+		System.out.println("Login Pag2");
 	}
 
 }
---
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -0,0 +1,10 @@
+package com.qa.pages;
+
+public class SearchPage {
+	
+	public void Search_Page()
+	{
+		System.out.println("Searcj Page1");
+	}
+
+}
